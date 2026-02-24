4c518e1b9ca241a10ca27f98d34118a416fff2b1
kirubeldess <kirubeldesalegn17@gmail.com>
2026-02-24 09:47:54 +0300

    fix: fixed status bar issue

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,10 +3,24 @@ import Toast from 'react-native-toast-message';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { MapThemeProvider } from '../modules/map/context/MapThemeContext';
+import { useEffect } from 'react';
+import * as NavigationBar from 'expo-navigation-bar';
+import { Platform } from 'react-native';
 import './globals.css';
 import '../shared/utils/localization/i18n';
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      try {
+        NavigationBar.setBackgroundColorAsync('#ffffff');
+        NavigationBar.setButtonStyleAsync('dark');
+      } catch (error) {
+        
+      }
+    }
+  }, []);
+
   return (
     <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
